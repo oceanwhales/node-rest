6bd57915fb93000eb3a59dec02b462c06587e0c8
sloudix <clement.cazeaux@gmail.com>
2016-03-13 19:08:05 +0000

    "first commit"

---
+++ client/js/app.js
@@ -0,0 +1,50 @@
+(function() {
+    /* global angular */
+    /* global _ */
+    var app = angular.module('store', []);
+
+    var data = {
+        text: 'hello',
+        phrase: ['how ', 'are ', 'you', '?'],
+        canRespond: true,
+        tags: [{
+            name: "Joli Film",
+            title: "joli film de fiction"
+        }]
+    };
+
+    var searchReview = {
+        results: []
+    }
+
+    app.controller('sayHello', ['$scope',function($scope) {
+
+        data.text += " you";
+        $scope.data = data;
+        return data;
+    }]);
+
+    app.controller('MediaController', function() {
+        this.newtag = {};
+
+        this.search = function(tagSeek) {
+            this.bfound = false;
+
+            var found = _.cloneDeep(_.find(data.tags, {
+                'name': tagSeek
+            }));
+            //            var found =
+            //            {
+            //                name: "toto",
+            //                title: "toto"
+            //            };
+            searchReview.results.push(found);
+            this.bfound = found;
+            this.newtag = found;
+
+        };
+
+        return searchReview;
+    });
+
+}());
