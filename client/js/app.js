6f6be5200487743f3361a690643fb58ba161f766
sloudix <clement.cazeaux@gmail.com>
2016-03-20 21:14:25 +0000

    clean client css

--- client/js/app.js
+++ client/js/app.js
@@ -1,50 +1,45 @@
 (function() {
-    /* global angular */
-    /* global _ */
-    var app = angular.module('store', []);
+   /* global angular */
+   /* global _ */
+   var app = angular.module('store', []);
 
-    var data = {
-        text: 'hello',
-        phrase: ['how ', 'are ', 'you', '?'],
-        canRespond: true,
-        tags: [{
-            name: "Joli Film",
-            title: "joli film de fiction"
-        }]
-    };
+   var data = {
+      text: 'hello',
+      phrase: ['how ', 'are ', 'you', '?'],
+      canRespond: true,
+      tags: [{
+         name: "Joli Film",
+         title: "joli film de fiction"
+      }]
+   };
 
-    var searchReview = {
-        results: []
-    }
+   var searchReview = {
+      results: []
+   }
 
-    app.controller('sayHello', ['$scope',function($scope) {
+   app.controller('sayHello', ['$scope', function($scope) {
 
-        data.text += " you";
-        $scope.data = data;
-        return data;
-    }]);
+      data.text += " you";
+      $scope.data = data;
+      return data;
+   }]);
 
-    app.controller('MediaController', function() {
-        this.newtag = {};
+   app.controller('MediaController', function() {
+      this.newtag = {};
 
-        this.search = function(tagSeek) {
-            this.bfound = false;
+      this.search = function(tagSeek) {
+         this.bfound = false;
 
-            var found = _.cloneDeep(_.find(data.tags, {
-                'name': tagSeek
-            }));
-            //            var found =
-            //            {
-            //                name: "toto",
-            //                title: "toto"
-            //            };
-            searchReview.results.push(found);
-            this.bfound = found;
-            this.newtag = found;
+         var found = _.cloneDeep(_.find(data.tags, {
+            'name': tagSeek
+         }));
+         searchReview.results.push(found);
+         this.bfound = found;
+         this.newtag = found;
 
-        };
+      };
 
-        return searchReview;
-    });
+      return searchReview;
+   });
 
 }());
